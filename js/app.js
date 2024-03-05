e402785ecd42f898386282141a97880953b07f7a
Michael-Chacon <michaelchacon02@gmail.com>
2024-03-05 15:27:36 -0500

    feat: :sparkles: WebComponent menu y footer
    Cree las clases para los webComponents  de menú de navegación y el footer, solo falta  agregar el HTML

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
-let nav = document.querySelector(".nav")
-let menu = document.querySelector('#tituloMenu')
-document.querySelector("#btnMenu").addEventListener("click",function(){
-    nav.classList.toggle("active")
-    menu.classList.toggle("hidden")
-})
+import "/App/webComponents/menu.js";
+import "/App/webComponents/footer.js";
+
+let nav = document.querySelector(".nav");
+let menu = document.querySelector("#tituloMenu");
+document.querySelector("#btnMenu").addEventListener("click", function () {
+  nav.classList.toggle("active");
+  menu.classList.toggle("hidden");
+});
